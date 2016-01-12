3669b20b0e9845b9fa2571740c01d28da68ca61f
rinne-grid <Rinne.Grids@gmail.com>
2016-01-12 22:16:34 +0900

    fix: パラメーターのバグを修正＆ウィンドウ項目名を指定可能とする

--- AnotherCurrencyShop/js/plugins/AnotherCurrencyShop.js
+++ AnotherCurrencyShop/js/plugins/AnotherCurrencyShop.js
@@ -1,19 +1,31 @@
 //=============================================================================
 // AnotherCurrencyShop.js
+//-----------------------------------------------------------------------------
+// Version
+// 1.0.0 プラグイン公開
+// 1.0.1 パラメーターのバグを修正＆購入ウィンドウコマンド項目名を指定可能とする
 //=============================================================================
 /*:ja
  * @plugindesc ゴールドではなく、変数の値で売買を行うショップを表示します
  * @author rinne_grid
  *
- * @param currencyName
+ * @param Currency Name
  * @desc 通貨の名称です。（ＭＰ、ＰＴ等を指定します）
  * @default ＰＴ
  *
- * @param variableNumber
+ * @param Variable Number
  * @desc 通貨として利用する変数の値です。(デフォルトでは1番目の変数が指定されます)
  * @default 1
  *
- * @ help
+ * @param Buy Command Name
+ * @desc アイテムを購入する際に表示するコマンド文字列です。必要に応じて「交換する」等を指定します
+ * @default 購入する
+ *
+ * @param Sell Command Name
+ * @desc アイテムを売却する際に表示するコマンド文字列です。
+ * @default 売却する
+ *
+ * @help
  *
  * プラグインコマンド
  *    AnotherCurrencyShop on    # ゴールドを使わないショップを有効にします
@@ -25,6 +37,8 @@
     var parameters = PluginManager.parameters('AnotherCurrencyShop');
     var currencyName = String(parameters['Currency Name'] || 'ＰＴ');
     var variableNumber = String(parameters['Variable Number'] || '1');
+    var buyCommandName = String(parameters['Buy Command Name'] || '購入する');
+    var sellCommandName = String(parameters['Sell Command Name'] || '売却する');
 
     //-------------------------------------------------------------------------
     // 関数退避
@@ -36,6 +50,7 @@
     var _Scene_Shop_doSell = Scene_Shop.prototype.doSell;
     var _Window_Gold_value = Window_Gold.prototype.value;
     var _Window_Gold_currencyUnit = Window_Gold.prototype.currencyUnit;
+    var _Window_ShopCommand_makeCommandList = Window_ShopCommand.prototype.makeCommandList;
 
 
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
@@ -78,6 +93,18 @@
         Window_Gold.prototype.currencyUnit = function() {
             return currencyName;
         };
+
+        Window_ShopCommand.prototype.makeCommandList = function() {
+            this.clearCommandList();
+
+            console.log(buyCommandName);
+            console.log(sellCommandName);
+
+            this.addCommand(buyCommandName,    'buy');
+            this.addCommand(sellCommandName,   'sell',   !this._purchaseOnly);
+            this.addCommand(TextManager.cancel, 'cancel');
+        };
+
     };
 
     //-------------------------------------------------------------------------
@@ -98,6 +125,10 @@
 
         Window_Gold.prototype.currencyUnit = function() {
             return _Window_Gold_currencyUnit.call(this);
+        };
+
+        Window_ShopCommand.prototype.makeCommandList = function() {
+            _Window_ShopCommand_makeCommandList.call(this);
         };
 
     };
